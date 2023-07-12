9feffbb3f2e78d124c1e67a7de007c93644401f5
stiven jimenez <sajqprogramming@gmail.com>
2023-07-12 16:48:13 -0500

    💄 add custom color in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      "deep-night": "#18171F ",
+      "pearl-gray": "#E6E5EA",
+      "aged-lavender": "#817D92 ",
+      "blueish-midnight": "#24232C",
+      "mint-green": "#A4FFAF",
+      "coral-red": "#F64A4A",
+      "sunset-peach": "#FB7C58",
+      "honey-yellow": "#F8CD65",
+    },
   },
   plugins: [],
 };
