3f0c5a7428dff2f096c0d05b18ccbcb70e0f6394
stiven jimenez <sajqprogramming@gmail.com>
2023-07-18 19:50:02 -0500

    💄 add JetBrainsMono font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jetbrains: ["JetBrainsMono", "monospace"],
+      },
+    },
     colors: {
       "deep-night": "#18171F ",
       "pearl-gray": "#E6E5EA",
